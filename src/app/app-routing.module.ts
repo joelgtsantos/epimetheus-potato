926199c526be66faf20012f757727df1ab0c6f79
Joel Santos <joelgtsantos@gmail.com>
2021-07-25 12:38:48 -0600

    Add new item component and basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NewItemComponent } from './inventory/new-item/new-item.component';
+
+const routes: Routes = [
+  { path: '', component: WelcomeComponent},
+  { path: 'newitem', component: NewItemComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NewItemComponent } from './inventory/new-item/new-item.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewItemComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
